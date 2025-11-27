0c550f5cfcb4fe78175b7dd50ab567ebbbd1790c
Danial Ali <danialali@mbp-de-danial.home>
2025-11-27 11:28:38 -0500

    Updated the parameters being queried when fetching upcoming events

--- src/components/eventPill.tsx
+++ src/components/eventPill.tsx
@@ -9,16 +9,20 @@ export default function EventPill({
 }: Readonly<EventPillAttributes>) {
   const eventDate = new Date(upcomingEvent.start_date);
   return (
-    <div className="border rounded-xl p-3 min-h-[300px]">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 event-pill items-center">
-        <div className="col-span-1">
-          <Image
-            height={300}
-            width={200}
-            src={upcomingEvent.poster_url}
-            alt={upcomingEvent.poster_alt}
-          />
-        </div>
+    <div className="border rounded-xl p-3 min-h-[325px]">
+      <div
+        className={`grid grid-cols-1 ${upcomingEvent.poster_url && "sm:grid-cols-2"} gap-4 event-pill items-center`}
+      >
+        {upcomingEvent.poster_url && (
+          <div className="col-span-1">
+            <Image
+              height={300}
+              width={200}
+              src={upcomingEvent.poster_url}
+              alt={upcomingEvent.poster_alt}
+            />
+          </div>
+        )}
         <div className="col-span-1">
           <p className="px-2 text-lg">{upcomingEvent.title}</p>
           <p className="px-2 text-lg">{upcomingEvent.location}</p>
@@ -32,11 +36,15 @@ export default function EventPill({
               minute: "2-digit",
             })}
           </p>
-          <p>
-            <button className="rounded py-4 px-2 hover:bg-[var(--secondary-colour-green-light)] hover:text-white text-[var(--main-colour-blue)] transition-colors">
-              <a href={upcomingEvent.registration_link}>Register here</a>
-            </button>
-          </p>
+          {upcomingEvent.call_to_action_link && (
+            <p>
+              <button className="rounded py-4 px-2 hover:bg-[var(--secondary-colour-green-light)] hover:text-white text-[var(--main-colour-blue)] transition-colors">
+                <a href={upcomingEvent.call_to_action_link}>
+                  {upcomingEvent.call_to_action_caption}
+                </a>
+              </button>
+            </p>
+          )}
         </div>
       </div>
     </div>
